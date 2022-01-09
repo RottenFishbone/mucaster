e012ec2e5607de95691b9a37b384c776580bae0a
Jayden <rottenfishbone@pm.me>
2022-01-09 14:07:55 -0700

    Moved dist to webapp for clarity. Adjusted server/mod.rs accordingly Added basic api route

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,10 +1,25 @@
 use std::path::Path;
 use tokio::sync::oneshot::Receiver;
+use warp::Filter;
 
-pub async fn host_webapp(port: u16, shutdown_rx: Receiver<()>) {
-    let route = warp::fs::dir("dist");
+pub async fn host_api(port: u16, shutdown_rx: Receiver<()>) {
+
+    // api serves as the main 
+    let api = warp::path("api").and(
+        warp::path("test").map(|| indoc::indoc!{"
+        {
+            'data': 'Api is working!'   
+        }"})
+    );
+    
+    let routes = warp::get().and(
+        warp::fs::dir("webapp")
+            .or(api)
+    );
+
+
     let addr = ([0,0,0,0], port);
-    let (_, server) = warp::serve(route)
+    let (_, server) = warp::serve(routes)
         .bind_with_graceful_shutdown(addr, async {
             shutdown_rx.await.ok();
         });
@@ -12,6 +27,8 @@ pub async fn host_webapp(port: u16, shutdown_rx: Receiver<()>) {
     server.await;
 }
 
+
+#[allow(dead_code)]
 pub async fn host_media(file: &Path, port: u16, shutdown_rx: Receiver<()>) {
     let route = warp::fs::file(file.to_path_buf());
     let addr = ([0,0,0,0], port);
